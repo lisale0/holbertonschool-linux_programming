636ab154745ec2f676592c2d1e7f5c8edb7d4a38
root <110@holbertonschool.com>
2018-06-11 05:32:04 +0000

    fix encoding issue

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -5,7 +5,7 @@ import sys
 def print_memory_of_pid(pid, only_writable=True):
 
     maps_file = open("/proc/{}/maps".format(sys.argv[1]), 'r')
-    mem_file = open("/proc/{}/mem".format(sys.argv[1]), 'r', 0)
+    mem_file = open("/proc/{}/mem".format(sys.argv[1]), 'rb', 0)
 
     memory_permissions = 'rw' if only_writable else 'r-'
     for line in maps_file.readlines():  # for each mapped region
@@ -17,9 +17,9 @@ def print_memory_of_pid(pid, only_writable=True):
             print("Start: {}, End: {}".format(start_addr,end_addr))
             mem_file.seek(start_addr)
             chunk = mem_file.read(end_addr - start_addr) # read region contents
-            find = chunk.find("Holberton")
+            find = chunk.find("Holberton".encode())
             
-            print(chunk)
+            print("----find: {}".format(find))
             
             print("[*] maps: /proc/{}/maps".format(sys.argv[1]) + "\n"
                   "[*] mem: /proc/{}/mem".format(sys.argv[1]) + "\n"
