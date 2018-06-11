625e06f8c3c248a5f5a7fb55f19a2409fbd322a5
root <110@holbertonschool.com>
2018-06-11 05:51:04 +0000

    comment

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -5,22 +5,31 @@ import sys
 def print_memory_of_pid(pid, only_writable=True):
 
     maps_file = open("/proc/{}/maps".format(sys.argv[1]), 'r')
-    mem_file = open("/proc/{}/mem".format(sys.argv[1]), 'rb', 0)
-
+    mem_file = open("/proc/{}/mem".format(sys.argv[1]), 'rb+', 0)
     memory_permissions = 'rw' if only_writable else 'r-'
-    for line in maps_file.readlines():  # for each mapped region
+
+    """ for each mapped region"""
+    for line in maps_file.readlines():
         spl = line.split(" ")
         addr = re.match(r'([0-9A-Fa-f]+)-([0-9A-Fa-f]+) ([-r][-w])', line)
-        if addr.group(3) == memory_permissions:
-            start_addr = int(addr.group(1), 16) #change to hex
+        if addr.group(3) == memory_permissions and "heap" in line:
+            """ Change to Base-16 """
+            start_addr = int(addr.group(1), 16)
             end_addr = int(addr.group(2), 16)
+
             print("Start: {}, End: {}".format(start_addr,end_addr))
+
+            """ Jump to the start position of the offset """
             mem_file.seek(start_addr)
-            chunk = mem_file.read(end_addr - start_addr) # read region contents
+            """ The chunk to look at"""
+            chunk = mem_file.read(end_addr - start_addr)
+
+            """ Find the string in the chunk """
             find = chunk.find("Holberton".encode())
-            
-            print("----find: {}".format(find))
-            
+                    
+            print(chunk)
+            print("-------->>>{}".format(line))
+            print("-------->>> start address: {}, end address: {}".format(start_addr, end_addr))
             print("[*] maps: /proc/{}/maps".format(sys.argv[1]) + "\n"
                   "[*] mem: /proc/{}/mem".format(sys.argv[1]) + "\n"
                   "Found [heap]:" + "\n"
